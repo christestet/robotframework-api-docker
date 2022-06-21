f8c47b6b17b43df7e9a5648e821ffe83159a9b26
Chris Testet Software <25032451+christestet@users.noreply.github.com>
2022-06-21 22:24:16 +0200

    update directory

--- docker-run-tests.sh
+++ docker-run-tests.sh
@@ -1,7 +1,7 @@
   #!/usr/bin/env bash
    docker run --rm \
   --user $(id -u):$(id -g) \
-  -v $PWD/tests:/home/robot/tests \
-  -v $PWD/results:/home/robot/results \
-  -v $PWD/libs:/home/robot/libs \
+  -v $PWD/tests:/root/tests \
+  -v $PWD/results:/root/results \
+  -v $PWD/libs:/root/libs \
   docker-api-robotframework
